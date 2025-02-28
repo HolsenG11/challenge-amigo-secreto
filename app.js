c9b2add92392b705962571454424b25c68f752e0
Felix Zarate <holsenzarate70@gmail.com>
2025-02-27 21:01:55 -0500

    agregando algunas validacione

--- app.js
+++ app.js
@@ -3,79 +3,79 @@
 let listaAmigosSorteados = [];
 
 function agregarAmigo() {
-    //obteniendo el nombre del amigo --- .trim() para que solo capture el nombre sin espacios    
-    let nombreAmigo = document.getElementById('amigo').value.trim();
+  //obteniendo el nombre del amigo --- .trim() para que solo capture el nombre sin espacios
+  let nombreAmigo = document.getElementById("amigo").value.trim();
 
-    if (nombreAmigo === "") {
-        alert("Por favor, ingrese un nombre");
+  if (nombreAmigo === "") {
+    alert("Por favor, ingrese un nombre");
+  } else {
+    if (listaAmigosSorteados.includes(nombreAmigo)) {
+      alert(
+        "Por favor, ingrese su segundo nombre o un apodo, ya que este nombre que desea ingresar, ya se encuentra en la lista"
+      );
+      LimpiarCaja();
     } else {
-        listaAmigosSorteados.push(nombreAmigo);    
-        LimpiarCaja();
-        actualizarLista();
-    }    
+      listaAmigosSorteados.push(nombreAmigo);
+      LimpiarCaja();
+      actualizarLista();
+    }
+  }
 
-    //validando que la lista se este agregando correctamente 
-    console.log(listaAmigosSorteados);
-
-    return;
+  //validando que la lista se este agregando correctamente
+  console.log(listaAmigosSorteados);
+  return;
 }
 
-function LimpiarCaja () {
-    document.querySelector('#amigo').value = '';
-    return;
+function LimpiarCaja() {
+  document.querySelector("#amigo").value = "";
+  return;
 }
 
 function actualizarLista() {
-    let lista = document.getElementById('listaAmigos');
-    lista.innerHTML = ""; // Limpiando la lista para evitar duplicados
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = ""; // Limpiando la lista para evitar duplicados
 
-    listaAmigosSorteados.forEach(amigo => {
-        let li = document.createElement('li'); // Agregando un nuevo elemento <li>
-        li.textContent = amigo; // Asignamos el nombre del amigo
-        lista.appendChild(li); // Agregando el elemento <li> a la lista <ul>
-    });
+  listaAmigosSorteados.forEach((amigo) => {
+    let li = document.createElement("li"); // Agregando un nuevo elemento <li>
+    li.textContent = amigo; // Asignamos el nombre del amigo
+    lista.appendChild(li); // Agregando el elemento <li> a la lista <ul>
+  });
 
-    return;
+  return;
 }
 
 function sortearAmigo() {
-    if (listaAmigosSorteados.length === 0) {
-        alert("No existe una lista de amigos para sortear.");
-        return;
-    } else if (listaAmigosSorteados.length == 1) {
-        alert("Debe de ingresar más de un nombre para empezar a sortear.");
-        return;
-    } else {
-        
-        //obteniendo el indice del amigo que salió ganador
-        let indiceGanador = Math.floor(Math.random() * listaAmigosSorteados.length);
-        let ganador = listaAmigosSorteados[indiceGanador];
+  if (listaAmigosSorteados.length === 0) {
+    alert("No existe una lista de amigos para sortear.");
+    return;
+  } else if (listaAmigosSorteados.length == 1) {
+    alert("Debe de ingresar más de un amigo para empezar a sortear.");
+    return;
+  } else {
+    //obteniendo el indice del amigo que salió ganador
+    let indiceGanador = Math.floor(Math.random() * listaAmigosSorteados.length);
+    let ganador = listaAmigosSorteados[indiceGanador];
 
-        //valida si lo puede mostrar de manera directa o necesitamos crear un <li>
-        document.getElementById('resultado').textContent = "🎉 El amigo secreto es: " + ganador + " 🎉";
+    //valida si lo puede mostrar de manera directa o necesitamos crear un <li>
+    document.getElementById("resultado").textContent =
+      "🎉 El amigo secreto es: " + ganador + " 🎉";
 
-        return;
-    }
-
+    //eliminando de la lista el amigo que ya ha sido sorteado y actualizando la lista
+    //VALIDAR QUE APAREZCA DURANTE UN TIEMPO EL GANADOR o ocultar cuando vas a agregar y si seguis sorteando dejarlo igual
+    listaAmigosSorteados.splice(indiceGanador, 1);
+    actualizarLista();
+    return;
+  }
 }
 
-//VALIDACIONES
+/* VALIDACIONES
 
-// tienes que validar que el campo no se encuentre vació y que lanza la alerta 
+// Validar si se debe Limpiar la lista para poder volver a sortear o se debe poder seguir agregando y sorteando
 
-// validar que no se agregue valores vacios al array 
-
-////
-
-// actualizar la lista de amigos 
-
-//sortear amigos
+//reiniciar juego 
 
 //Validar que no se pueda ingresar ningún número y ver si podemos corregir lo de caractéres especiales 
 
 //Ver si podemos cambiar algunos mensajes a que se visualicen en el lu y no en el alert
 
-// Validar si se debe Limpiar la lista para poder volver a sortear o se debe poder seguir agregando y sorteando
-
-// Validar que no se pueda ingresar dos nombres igulaes, ya que puede que exista una confusión. Mejor enviar una alerta para volver a ingresar otro nombre
-// o al inició inabilitar el botón de sortear para que se habilite cuando ya hay un registro
+*/
